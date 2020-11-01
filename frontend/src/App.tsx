3a904ed7be805a9636435af91921e19f40fe6d44
Goel Biju <goelmolel@hotmail.com>
2020-11-01 23:51:51 +0000

    Add Documents folder with initial project documentation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+import ScatterGraph from './graphs/lineChart.component';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Test</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <p>Visualising Optimisation Data</p>
+            <div>
+                <ScatterGraph />
+            </div>
         </div>
     );
 }
---
+++ frontend/src/graphs/lineChart.component.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LineChart = (props: unknown): React.ReactElement => {
+    const data = [
+        { x: 0, y: 8 },
+        { x: 1, y: 5 },
+        { x: 2, y: 4 },
+        { x: 3, y: 9 },
+        { x: 4, y: 1 },
+        { x: 5, y: 7 },
+        { x: 6, y: 6 },
+        { x: 7, y: 3 },
+        { x: 8, y: 2 },
+        { x: 9, y: 0 },
+    ];
+
+    return <div>Test</div>;
+};
+
+export default LineChart;

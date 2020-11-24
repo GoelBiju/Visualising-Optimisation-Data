1c3922a5fae808d13d72d84b70a0779fc2ddb0b5
Goel Biju <goelmolel@hotmail.com>
2020-11-24 09:17:05 +0000

    Display scatter plot only on frontend (#2)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,90 @@
+import {
+    AppBar,
+    CssBaseline,
+    Divider,
+    Drawer,
+    List,
+    ListItem,
+    ListItemText,
+    Toolbar,
+    Typography,
+} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import React from 'react';
 import './App.css';
-import BarChart from './graphs/bar/barChart.component';
-import DetailBarChart from './graphs/bar/detailBarChart.component';
 import DetailScatterPlot from './graphs/scatter/detailScatterPlot.component';
-import ScatterPlot from './graphs/scatter/scatterPlot.component';
+
+const drawerWidth = 200;
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+        },
+        appBar: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        toolbar: theme.mixins.toolbar,
+        content: {
+            flexGrow: 1,
+            backgroundColor: theme.palette.background.default,
+            padding: theme.spacing(3),
+        },
+    }),
+);
 
 function App() {
+    const classes = useStyles();
+
+    const [selectedScatter, setSelectedScatter] = React.useState(true);
+
     return (
         <div className="App">
-            <p>Visualising Optimisation Data</p>
-            <BarChart />
-            <ScatterPlot />
-            <DetailScatterPlot />
-            <DetailBarChart />
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <Typography variant="h6" noWrap>
+                            Visualising Optimisation Data
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+                <Drawer
+                    className={classes.drawer}
+                    variant="permanent"
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    anchor="left"
+                >
+                    <div className={classes.toolbar} />
+                    <Divider />
+                    <List>
+                        {['Scatter Plot'].map((text) => (
+                            <ListItem button onClick={() => setSelectedScatter(true)} key={text}>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
+
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    {/* <BarChart /> */}
+                    {/* <ScatterPlot /> */}
+                    {selectedScatter && <DetailScatterPlot />}
+                    {/* <DetailBarChart /> */}
+                </main>
+            </div>
         </div>
     );
 }
--- frontend/src/graphs/scatter/detailScatterPlot.component.tsx
+++ frontend/src/graphs/scatter/detailScatterPlot.component.tsx
@@ -8,8 +8,21 @@ const DetailScatterPlot = (): React.ReactElement => {
         bottom: 30,
         left: 60,
     };
-    const width = 460 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    const width = 760 - margin.left - margin.right;
+    const height = 800 - margin.top - margin.bottom;
+
+    const dataset = [
+        [34, 78],
+        [109, 280],
+        [310, 120],
+        [79, 411],
+        [420, 220],
+        [233, 145],
+        [333, 96],
+        [222, 333],
+        [78, 320],
+        [21, 123],
+    ];
 
     // Create ref for svg chart.
     const chartRef = React.createRef<SVGSVGElement>();
@@ -26,30 +39,38 @@ const DetailScatterPlot = (): React.ReactElement => {
             .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
         // Read data.
-        d3.csv('https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv').then(
-            (data) => {
-                console.log(data);
+        // d3.csv('https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv').then(
+        //     (data) => {
+        //         console.log(data);
 
-                // Add x axis
-                const x = d3.scaleLinear().domain([0, 3000]).range([0, width]);
-                svg.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
+        // Add x axis
+        const x = d3
+            .scaleLinear()
+            .domain([0, d3.max(dataset, (d) => d[0]) as number])
+            .range([0, width]);
+        svg.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
 
-                // Add y axis
-                const y = d3.scaleLinear().domain([0, 400000]).range([height, 0]);
-                svg.append('g').call(d3.axisLeft(y));
+        // Add y axis
+        const y = d3
+            .scaleLinear()
+            .domain([0, d3.max(dataset, (d) => d[1]) as number])
+            .range([height, 0]);
+        svg.append('g').call(d3.axisLeft(y));
 
-                // Add dots
-                svg.append('g')
-                    .selectAll('dot')
-                    .data(data)
-                    .enter()
-                    .append('circle')
-                    .attr('cx', (d) => x(Number(d.GrLivArea)) as number)
-                    .attr('cy', (d) => y(Number(d.SalePrice)) as number)
-                    .attr('r', 1.5)
-                    .style('fill', '#69b3a2');
-            },
-        );
+        // Add dots
+        svg.append('g')
+            .selectAll('dot')
+            .data(dataset)
+            .enter()
+            .append('circle')
+            // .attr('cx', (d) => x(Number(d.GrLivArea)) as number)
+            // .attr('cy', (d) => y(Number(d.SalePrice)) as number)
+            .attr('cx', (d) => d[0])
+            .attr('cy', (d) => d[1])
+            .attr('r', 1.5)
+            .style('fill', '#69b3a2');
+        //     },
+        // );
     }, [chartRef]);
 
     return (

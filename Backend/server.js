53ec6fe73889867bf10e5dcc9060dd95c3bfe5bd
Goel Biju <goelmolel@hotmail.com>
2020-11-24 09:16:34 +0000

    Add Socket.io to backend (#2)

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,42 @@
-var mongoose = require("mongoose");
+let express = require("express");
+let path = require("path");
+let http = require("http");
+let socketIo = require("socket.io");
+
+// var mongoose = require("mongoose");
 
 // Connection information.
 
 // Define a scheme.
-var Schema = mongoose.Schema;
+// var Schema = mongoose.Schema;
 
-var SomeModelSchema = new Schema({
-  a_string: String,
-  a_date: Date,
+// var SomeModelSchema = new Schema({
+//   a_string: String,
+//   a_date: Date,
+// });
+
+// Set up the app and server.
+let app = express();
+let server = http.createServer(app);
+
+// Configure statics.
+app.use(express.static(path.join(__dirname, "resources")));
+
+// Initialise the socket server.
+let io = socketIo(server);
+
+// "On connection" handler
+io.on("connection", function (socket) {
+  console.log("received connection");
+
+  // TODO: Send a message to the client.
+  // socket.emit("server message", "Hello World");
+
+  socket.on("dataPoint", function (msg) {
+    console.log("Received data point: ", msg);
+  });
+});
+
+server.listen(9000, () => {
+  console.log("Listening on 9000");
 });

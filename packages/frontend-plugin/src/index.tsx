372d3a8d2d0647bb430cb2a2f9dfc9a180ed1315
Goel Biju <goelmolel@hotmail.com>
2021-02-12 15:07:16 +0000

    Add TODO (#17)

--- packages/frontend-plugin/src/index.tsx
+++ packages/frontend-plugin/src/index.tsx
@@ -7,6 +7,7 @@ import "./index.css";
 
 const pluginName = "frontend-plugin";
 
+// TODO: Does this render function work?
 const render = (): void => {
   console.log("Calling render");
   let el = document.getElementById(pluginName);

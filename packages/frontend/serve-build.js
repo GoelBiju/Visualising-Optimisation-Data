7b7a7d9a2192c9e268f1ae2bd05ff3e9f8096f99
Goel Biju <16869075+GoelBiju@users.noreply.github.com>
2021-03-14 01:35:33 +0000

    Remove serve from frontend

--- packages/frontend/serve-build.js
+++ packages/frontend/serve-build.js
@@ -1,3 +1,6 @@
+// This file is to manually serve the build folder on Heroku.
+// This is specifically to get around the issue of the
+// plugin not rendering with a react router link.
 const express = require('express');
 const path = require('path');
 
@@ -6,6 +9,8 @@ const PORT = process.env.PORT || 9000;
 
 app.use(express.static(path.join(__dirname, '/build')));
 
+// We direct any GET request to the index.html to handle,
+// to avoid Heroku returning a 404 when we try to load the data page.
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '/build', 'index.html'));
 });

51cc8cfd964d335b5bac624bc5efdb58cd859aa8
Goel Biju <goelmolel@hotmail.com>
2021-07-31 00:48:15 +0100

    Correct cast Plotly data (#57)

--- packages/three-obj/src/visualisation.component.tsx
+++ packages/three-obj/src/visualisation.component.tsx
@@ -1,12 +1,27 @@
 import { Data, StateType } from "frontend-common";
 import React from "react";
-import { connect } from "react-redux";
-
-// import Plot from "react-plotly.js";
+// import { connect } from "react-redux";
 
 import createPlotlyComponent from "react-plotly.js/factory";
 const Plotly = window.Plotly;
 const Plot = createPlotlyComponent(Plotly);
+
+/**
+ * Create two arrays num length with random values from 0 to mul
+ *
+ * @param {*} num
+ * @param {*} mul
+ * @returns
+ */
+// function randomValues(num: number, mul: number) {
+//   const arr = [];
+//   const index = [];
+//   for (let i = 0; i < num; i++) {
+//     arr.push(Math.random() * mul);
+//     index.push(i);
+//   }
+//   return { index, arr };
+// }
 
 interface VCProps {
   data: Data;
@@ -14,22 +29,58 @@ interface VCProps {
 
 const VisualisationComponent = (): React.ReactElement => {
   // props: VCProps
+
+  // const data = [
+  //   {
+  //     x: [1, 2, 3],
+  //     y: [2, 6, 3],
+  //     type: "scatter",
+  //     mode: "lines+markers",
+  //     marker: { color: "red" },
+  //   },
+  //   { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+  // ];
+
+  // const data = [
+  //   {
+  //     x: [1, 2, 3],
+  //     y: [2, 6, 3],
+  //     type: "scatter",
+  //     mode: "lines+markers",
+  //     marker: { color: "red" },
+  //   },
+  //   { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+  // ] as Plotly.Data[];
+
+  var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: "markers",
+    type: "scatter",
+  };
+
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: "lines",
+    type: "scatter",
+  };
+
+  var trace3 = {
+    x: [1, 2, 3, 4],
+    y: [12, 9, 15, 12],
+    mode: "lines+markers",
+    type: "scatter",
+  };
+
+  var data = [trace1, trace2, trace3] as Plotly.Data[];
+
   return (
     <div id="chartContainer">
       <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: "scatter",
-            mode: "lines+markers",
-            marker: { color: "red" },
-          },
-          { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
-        ]}
+        data={data}
         layout={{ width: 500, height: 500, title: "A Fancy Plot" }}
       />
-      {/* Test */}
     </div>
   );
 };
